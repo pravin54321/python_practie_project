1a8bdad357c92375b99e94749e73aca2271ec561
pravin <mendhe.pravin123@gmail.com>
2022-10-04 08:41:49 +0530

    factorial_of_number

---
+++ factorial.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+num=int(input("Enter The Number"))
+for i in range(1,num//2+1):
+   
+    if num%i==0:
+        print(i)
+        
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
