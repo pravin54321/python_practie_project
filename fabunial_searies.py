4e6e6254a6d0ce31771aa8fbc77026dc38671d2e
pravin <mendhe.pravin123@gmail.com>
2022-10-10 09:19:37 +0530

    fabinual searies

---
+++ fabunial_searies.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+def fib(n):
+    a = 0
+    b = 1
+    if n == 1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        for i in range(2,n):
+            c = a + b
+            a = b
+            b = c
+            print(c)
+fib(10)
+
+
+# In[18]:
+
+
+def feb(n):
+    a=0
+    b=1
+    if n==1:
+        print(0)
+    else:
+        print(a)
+        print(b)
+        for i in range(2,n):
+            c=a+b
+            a=b
+            b=c
+            print(c)    
+        
+print(feb(8))      
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
