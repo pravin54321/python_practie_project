d18e4848a0a7be94e0ddf6cb67f7de5913cda4f0
pravin <mendhe.pravin123@gmail.com>
2022-10-11 09:50:17 +0530

    star_patern problems

---
+++ start_pattern.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+num=10
+for  i in range(1,10):
+    print(i*"*")
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[26]:
+
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print("*",end="")
+    print("\r")    
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
