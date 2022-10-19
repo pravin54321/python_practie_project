7932a90abee7f6ba11c223a84cc78d5bf463f6c8
pravin <mendhe.pravin123@gmail.com>
2022-10-19 09:50:27 +0530

    array rotation

---
+++ array_rotation.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+arr=[1,2,3,4,5,6,7]
+num=int(input("enter number"))
+a=[]
+for i in range(0,num):
+    a.append(arr[i])
+    
+z=arr[num:]+a
+print(z)
+
+
+# In[4]:
+
+
+ary1=[1,2,3,4,5,6,7]
+l=len(ary1)-1
+g=[]
+while l>=0:
+    g.append(ary1[l])
+    l -= 1
+
+print(g)  
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
