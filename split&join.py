d4577cc148390b3056dc2f89757149868ec9e3ee
pravin <mendhe.pravin123@gmail.com>
2022-11-17 10:42:28 +0530

    split&join

---
+++ split&join.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+string="geek for geek"
+def substring(string):
+    s=string.split(' ')
+    return s
+print(substring(string))
+
+
+# In[4]:
+
+
+string1="geek for geek"
+def jnstr(string1):
+    s1='-'.join(string1)
+    return s1
+print(jnstr(string1))
+
+
+# In[6]:
+
+
+string3="geek for geek"
+s3="-".join(string3.split(" "))
+print(s3)
+
+
+# In[ ]:
+
+
+
+
